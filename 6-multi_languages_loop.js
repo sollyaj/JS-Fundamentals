5bb4814b127afbc592c497606e03a9bb13019ef3
sollyaj <soajohworode@gmail.com>
2025-07-08 16:09:16 +0100

    Add 6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const length = languages.length;
+let index = 0;
+
+while (index < length) {
+    console.log(languages[index]);
+    index++;
+}
