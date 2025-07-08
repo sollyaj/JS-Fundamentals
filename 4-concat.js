40fa6026abd3b6f95a82bcbf88eddbfba7654426
sollyaj <soajohworode@gmail.com>
2025-07-08 15:49:02 +0100

    Change the update

--- 4-concat.js
+++ 4-concat.js
@@ -1,15 +1,5 @@
-// Get the list of arguments passed to the script
-const args = process.argv.slice(2); // slice(2) to ignore the first two elements
+const args = process.argv.slice(2); // Get command-line arguments
+const firstArg = args[0];
+const secondArg = args[1];
 
-// Check if at least one argument is passed
-if (args[0]) {
-    if (args[0] === "c") {
-        console.log("c cool");
-        console.log("c is cool");
-    } else {
-        console.log(`${args[0]} cool`);
-        console.log(`${args[0]} is cool`);
-    }
-} else {
-    console.log("Please provide an argument.");
-}
+console.log(firstArg + " is " + secondArg);
