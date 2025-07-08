402cb851066b5a4d855064cddf72e875c09da2fb
sollyaj <soajohworode@gmail.com>
2025-07-08 15:42:54 +0100

    Update the changes made

--- 4-concat.js
+++ 4-concat.js
@@ -3,8 +3,13 @@ const args = process.argv.slice(2); // slice(2) to ignore the first two elements
 
 // Check if at least one argument is passed
 if (args[0]) {
-    console.log(`${args[0]} cool`);
-    console.log(`${args[0]} is cool`);
+    if (args[0] === "c") {
+        console.log("c cool");
+        console.log("c is cool");
+    } else {
+        console.log(`${args[0]} cool`);
+        console.log(`${args[0]} is cool`);
+    }
 } else {
     console.log("Please provide an argument.");
 }
