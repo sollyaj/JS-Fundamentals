d96a1c1e47338a623ddbc41a5068c6ca59d0fbc2
sollyaj <soajohworode@gmail.com>
2025-07-08 16:22:34 +0100

    Update the changes made

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,13 +1,13 @@
-const args = process.argv.slice(2);
-const x = Number(args[0]);
-const isInteger = Number.isInteger(x);
 
-if (!isInteger || x < 1) {
-    console.log("Missing number of occurrences");
+
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
 } else {
-    let count = 0;
-    while (count < x) {
-        console.log("C is fun");
-        count++;
-    }
+  let i = 0;
+  while (i < x) {
+    console.log("C is fun");
+    i++;
+  }
 }
